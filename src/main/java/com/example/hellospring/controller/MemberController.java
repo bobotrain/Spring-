b274758112f095ccb8db89aa3b27e76982eedc69
최경보 <chlrudqh97@naver.com>
2023-05-31 23:49:38 +0900

    first commit (2023.05.07) member 코드 작성 second commit (2023.05.09) testcode 작성 third commit (2023.05.10) 서비스 코드 작성 4 commit(2023.05.11) 서비스 코드 testcode 작성 5 commit(2023.05.12) 서비스 코드 testcode 작성 6 commit(2023.05.12) spring 자동의존관계설정 7 commit(2023. 05. 29 ) spring 컴포넌트 방식 스캔과 자바 코드로 직접 스프링 빈 등록하기 / 회원 웹 기능 추가 ( 홈 화면 추가 ) 8 commit(2023. 05. 31 ) 등록 화면 추가

--- src/main/java/com/example/hellospring/controller/MemberController.java
+++ src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,9 +15,32 @@ public class MemberController {
 
     @Autowired //autowired를 써야 해당 서비스를 연결.
     public MemberController(MemberService memberService) {
+
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        //회원가입 눌렀을때 members/new로 오기 때문에 여기 매핑을 거침
+        //createMemberForm화면으로 이동
+        return "members/createMemberForm";
+    }
+
+    //members/createMemberForm에서 입력한 것들(input타입 안 text들) form태그를 통해 submit -> post방식으로 넘어옴
+    // 즉 여기 @PostMapping을 거치게 된다.
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+
+        //createMemberForm에서 setName을 통해 담아둔 name을 getName으로 호출하여 member에 담음 ( controller>MemberForm 참고 )
+        member.setName(form.getName());
+        
+        //join에 받은  member name을 받아 넣어둠
+        memberService.join(member);
+        
+        //회원가입 끝났으니 home화면으로 돌려보냄
+        return "redirect:/";
+    }
 
 
 }
---
+++ src/main/java/com/example/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package com.example.hellospring.controller;
+
+public class MemberForm {
+
+    //templates>members>createMemberFrom의 name과 매치
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
